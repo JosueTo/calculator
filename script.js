6adaaf5f1b64c993ba459115a1f246245a5c81db
Josue Torres <josuetolvera@gmail.com>
2022-09-30 17:56:35 -0500

    Add functions to perform math operations (+, -, *, /)

---
+++ script.js
@@ -0,0 +1,5 @@
+
+const add = (operand1, operand2) => operand1 + operand2;
+const subtract = (operand1, operand2) => operand1 - operand2;
+const multiply = (operand1, operand2) => operand1 * operand2;
+const divide = (operand1, operand2) => operand1 / operand2;
