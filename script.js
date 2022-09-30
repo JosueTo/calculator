aadb894d274cac0cc9b6beea9a0bb6eacfd3f681
Josue Torres <josuetolvera@gmail.com>
2022-09-30 18:20:00 -0500

    Add an operate function

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const add = (operand1, operand2) => operand1 + operand2;
 const subtract = (operand1, operand2) => operand1 - operand2;
 const multiply = (operand1, operand2) => operand1 * operand2;
 const divide = (operand1, operand2) => operand1 / operand2;
+
+const operate = (num1, operator, num2) => {
+  if (operator === '+') {
+    return add(num1, num2);
+  } else if (operator === '-') {
+    return subtract(num1, num2);
+  } else if (operator === '*') {
+    return multiply(num1, num2);
+  } else if (operator === '/') {
+    return divide(num1, num2);
+  }
+}
