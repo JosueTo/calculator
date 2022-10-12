57b40361cde8950576b8a65862b1c824de4bd598
Josue Torres <josuetolvera@gmail.com>
2022-10-11 20:12:26 -0500

    Add main function and handleNumbers function

--- script.js
+++ script.js
@@ -1,20 +1,62 @@
 
-const add = (operand1, operand2) => operand1 + operand2;
-const subtract = (operand1, operand2) => operand1 - operand2;
-const multiply = (operand1, operand2) => operand1 * operand2;
-const divide = (operand1, operand2) => operand1 / operand2;
+// const add = (operand1, operand2) => operand1 + operand2;
+// const subtract = (operand1, operand2) => operand1 - operand2;
+// const multiply = (operand1, operand2) => operand1 * operand2;
+// const divide = (operand1, operand2) => operand1 / operand2;
 
-let displayValue = 0;
+// let displayValue = 0;
 
 
-const operate = (num1, operator, num2) => {
-  if (operator === '+') {
-    return add(num1, num2);
-  } else if (operator === '-') {
-    return subtract(num1, num2);
-  } else if (operator === '*') {
-    return multiply(num1, num2);
-  } else if (operator === '/') {
-    return divide(num1, num2);
+// const operate = (num1, operator, num2) => {
+//   if (operator === '+') {
+//     return add(num1, num2);
+//   } else if (operator === '-') {
+//     return subtract(num1, num2);
+//   } else if (operator === '*') {
+//     return multiply(num1, num2);
+//   } else if (operator === '/') {
+//     return divide(num1, num2);
+//   }
+// }
+
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const allClear = document.querySelector('#all-clear');
+const undo = document.querySelector('.delete');
+const equals = document.querySelector('.equals');
+const previousOperand = document.querySelector('.previous-operation');
+const currentOperand = document.querySelector('.current-operation');
+
+let currentValue = '';
+let previousValue = '';
+let operator = '';
+
+// main function
+const calculator = () => {
+  // events
+  numbers.forEach(number => number.addEventListener('click', (e) => {
+      handleNumbers(e.target.textContent);
+      currentOperand.textContent = currentValue;
+      console.log(currentValue.length);
+  }))
+}
+
+// functions
+const handleNumbers = (number) => {
+  if (currentValue.length < 12) {
+    if (number === '.' && currentValue.includes('.')) return;
+    currentValue += number;
   }
 }
+
+calculator();
+
+// main function 
+// const calculator = () => {
+//   // events
+//   numbers.forEach(number => {
+//     number.addEventListener('click', () => {
+//       currentOperation.textContent += number.textContent;
+//     })
+//   })
+// }
